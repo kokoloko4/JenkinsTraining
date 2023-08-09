d686568a4d53a5bcb629ed4faea9c49a961efaf5
Andrés Cocunubo <anfecoquin@hotmail.com>
2023-08-09 14:20:09 -0500

    fix project 2

---
+++ src/main/java/com/andres/training/JenkinsCalculator.java
@@ -0,0 +1,12 @@
+package com.andres.training;
+
+public class JenkinsCalculator {
+
+    public int addNumbers(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtractNumbers(int num1, int num2) {
+        return num1 - num2;
+    }
+}
